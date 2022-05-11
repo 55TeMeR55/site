60389107b1b8e1f002168f4577dad0084f2dd574
Vad <temerev.vadim@gmail.com>
2022-05-11 22:16:01 +0600

    update for preview

---
+++ index.php
@@ -0,0 +1,112 @@
+<?
+
+	//конект к базе
+	$link = mysqli_connect("localhost","root","root","sql_database");
+	//проверяем куки
+	if (isset($_COOKIE['id']) and isset($_COOKIE['hash']))
+	{//включаем бутон
+		$button=true;
+
+//выборку по кукам
+		$query = mysqli_query($link, "SELECT *,INET_NTOA(user_ip) AS user_ip FROM users WHERE id = '".intval($_COOKIE['id'])."' LIMIT 1");
+		$userdata = mysqli_fetch_assoc($query);
+		
+//обьявляем переменные
+		$role = $userdata['role'];
+		$username = $userdata['username'];
+	//ранг пользователя если админ то вкл админ панель
+		if($role==1)
+		{
+			$admin=true;
+		}
+	//обновляем куки
+		if(($userdata['user_hash'] !== $_COOKIE['hash']) or ($userdata['id'] !== $_COOKIE['id'])
+		or (($userdata['user_ip'] !== $_SERVER['REMOTE_ADDR'])  and ($userdata['user_ip'] !== "0")))
+		{
+			setcookie("id", "", time() - 3600*24*30*12, "/");
+			setcookie("hash", "", time() - 3600*24*30*12, "/", null, null, true); // httponly !!!
+			print "Хм, что-то не получилось";
+		}
+		else
+		{
+
+		}
+	}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Primer</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<body>
+<div class="header">
+  <h1>Site with different level access</h1>
+  <p>Log in and you will see the difference!</p> 
+  <?
+    If($button==true)
+  {
+	  ?>
+    <p>Welcome! You log in as "<?echo $username?>"</p> 
+	<?
+  }
+  ?>
+</div>
+
+<div class="navbar">
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+  <?If($admin==true)
+  {
+  echo "<a href='admin.php' class='right'>Admin panel of site</a>";
+  }
+  If($button==true)
+  {
+  echo "<a href='logout.php' class='right'>Logout with site</a>";
+  echo "<a href='profile.php' class='right'>Your prolile on site</a>";
+  }
+  else
+  {
+  echo "<a href='login.php' class='right'>Login on site</a>";
+  }
+  ?>
+</div>
+
+<div class="row">
+  <div class="side">
+      <h2>About Me</h2>
+      <h5>Photo of me:</h5>
+      <div class="fakeimg" style="height:200px;">Image</div>
+      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+      <h3>More Text</h3>
+      <p>Lorem ipsum dolor sit ame.</p>
+      <div class="fakeimg" style="height:60px;">Image</div><br>
+      <div class="fakeimg" style="height:60px;">Image</div><br>
+      <div class="fakeimg" style="height:60px;">Image</div>
+  </div>
+  <div class="main">
+      <h2>My blog</h2>
+      <h5>Thursday, March 4, 2021</h5>
+  <p><img src="images/base.jpg" 
+  width="500" height="300" alt="lorem"></a>
+      <p>Creating Database</p>
+      <p>Today I started creating the site database! </p>
+      <br>
+      <h2>TITLE HEADING</h2>
+      <h5>Monday, March 1, 2021</h5>
+  <p><img src="images/meet.png" 
+  width="600" height="300" alt="lorem"></a>
+      <p>New month new features </p>
+      <p>Today I went for an interview, like I went to SEO, I came to a web developer) </p>
+  </div>
+</div>
+
+</body>
+</html>
+
+
---
+++ register.php
@@ -0,0 +1,213 @@
+<?
+// Страница регистрации нового пользователя
+
+// Соединямся с БД
+$link=mysqli_connect("localhost","root","root","sql_database");
+if(isset($_POST['submit']))
+{
+
+	
+    $err = [];
+
+    // проверям логин
+    if(!preg_match("/^[a-zA-Z0-9]+$/",$_POST['login']))
+    {
+        $err[] = "Логин может состоять только из букв английского алфавита и цифр";
+    }
+
+    if(strlen($_POST['login']) < 3 or strlen($_POST['login']) > 30)
+    {
+        $err[] = "Логин должен быть не меньше 3-х символов и не больше 30";
+    }
+
+    // проверяем, не сущестует ли пользователя с таким именем
+    $query = mysqli_query($link, "SELECT id FROM users WHERE username='".mysqli_real_escape_string($link, $_POST['login'])."'");
+    if(mysqli_num_rows($query) > 0)
+    {
+        $err[] = "Пользователь с таким логином уже существует в базе данных";
+    }
+
+    // Если нет ошибок, то добавляем в БД нового пользователя
+    if(count($err) == 0)
+    {
+
+        $login = $_POST['login'];
+
+        // Убераем лишние пробелы и делаем двойное хеширование
+        $password = md5(md5(trim($_POST['password'])));
+		//добавляем в базу данные
+        mysqli_query($link,"INSERT INTO users SET username='".$login."', password='".$password."', role='2', dateregistration=NOW()");
+
+        header("Location: login.php"); exit();
+    }
+    else
+    {					
+	?>
+<p><center><?php print "<b>При регистрации произошли следующие ошибки:</b><br>"; ?></center></p>
+		<?
+        foreach($err AS $error)
+        {
+					?>
+<p><center><?php echo $error; ?></center></p>
+		<?
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+</head>
+<style>
+/* Demo Background */
+body{background:url(/images/bg/bg-6.png)}
+
+/* Form Style */
+.form-horizontal{
+ background: #fff;
+ padding-bottom: 40px;
+ border-radius: 15px;
+ text-align: center;
+}
+.form-horizontal .heading{
+ display: block;
+ font-size: 35px;
+ font-weight: 700;
+ padding: 35px 0;
+ border-bottom: 1px solid #f0f0f0;
+ margin-bottom: 30px;
+}
+.form-horizontal .form-group{
+ padding: 0 40px;
+ margin: 0 0 25px 0;
+ position: relative;
+}
+.form-horizontal .form-control{
+ background: #f0f0f0;
+ border: none;
+ border-radius: 20px;
+ box-shadow: none;
+ padding: 0 20px 0 45px;
+ height: 40px;
+ transition: all 0.3s ease 0s;
+}
+.form-horizontal .form-control:focus{
+ background: #e0e0e0;
+ box-shadow: none;
+ outline: 0 none;
+}
+.form-horizontal .form-group i{
+ position: absolute;
+ top: 12px;
+ left: 60px;
+ font-size: 17px;
+ color: #c8c8c8;
+ transition : all 0.5s ease 0s;
+}
+.form-horizontal .form-control:focus + i{
+ color: #00b4ef;
+}
+.form-horizontal .fa-question-circle{
+ display: inline-block;
+ position: absolute;
+ top: 12px;
+ right: 60px;
+ font-size: 20px;
+ color: #808080;
+ transition: all 0.5s ease 0s;
+}
+.form-horizontal .fa-question-circle:hover{
+ color: #000;
+}
+.form-horizontal .main-checkbox{
+ float: left;
+ width: 20px;
+ height: 20px;
+ background: #11a3fc;
+ border-radius: 50%;
+ position: relative;
+ margin: 5px 0 0 5px;
+ border: 1px solid #11a3fc;
+}
+.form-horizontal .main-checkbox label{
+ width: 20px;
+ height: 20px;
+ position: absolute;
+ top: 0;
+ left: 0;
+ cursor: pointer;
+}
+.form-horizontal .main-checkbox label:after{
+ content: "";
+ width: 10px;
+ height: 5px;
+ position: absolute;
+ top: 5px;
+ left: 4px;
+ border: 3px solid #fff;
+ border-top: none;
+ border-right: none;
+ background: transparent;
+ opacity: 0;
+ -webkit-transform: rotate(-45deg);
+ transform: rotate(-45deg);
+}
+.form-horizontal .main-checkbox input[type=checkbox]{
+ visibility: hidden;
+}
+.form-horizontal .main-checkbox input[type=checkbox]:checked + label:after{
+ opacity: 1;
+}
+.form-horizontal .text{
+ float: left;
+ margin-left: 7px;
+ line-height: 20px;
+ padding-top: 5px;
+ text-transform: capitalize;
+}
+.form-horizontal .btn{
+ float: right;
+ font-size: 14px;
+ color: #fff;
+ background: #00b4ef;
+ border-radius: 30px;
+ padding: 10px 40px;
+ border: none;
+ text-transform: capitalize;
+ transition: all 0.5s ease 0s;
+}
+@media only screen and (max-width: 479px){
+ .form-horizontal .form-group{
+ padding: 0 25px;
+ }
+ .form-horizontal .form-group i{
+ left: 45px;
+ }
+ .form-horizontal .btn{
+ padding: 10px 20px;
+ }
+}
+</style>
+<div class="container">
+ <div class="row">
+  <div class="col-md-offset-3 col-md-6">
+ <form method="POST" class="form-horizontal">
+ <span class="heading">Register</span>
+ <div class="form-group">
+ <input name="login" class="form-control" type="text" placeholder="Login" required>
+ <i class="fa fa-user"></i>
+ </div>
+ <div class="form-group help">
+ <input name="password" class="form-control" type="password" placeholder="Password" required>
+ <i class="fa fa-lock"></i>
+ <a href="#" class="fa fa-question-circle"></a>
+ </div>
+<input name="submit1" type="submit" class="btn btn-default" value="Register">
+ </form>
+ </div>
+ </div>
+</div>
+</form>
